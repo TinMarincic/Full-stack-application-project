0513badceee7c1948a9338bb7bb67983cc588028
TinMarincic <tinmarincic2004@gmail.com>
2024-08-20 21:26:34 +0200

    relocated mailer.ts file into contact folder

--- apps/web/src/app/contact/page.tsx
+++ apps/web/src/app/contact/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { useState } from 'react';
-import { sendmail } from '../booking/mailer';
+import { sendmail } from './mailer';
 
 export default function ContactUs() {
   const [formData, setFormData] = useState({
